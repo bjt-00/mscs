de959bf001c54e0d0603a48dd280af933c5359ca
thesuffah <bitguiders@gmail.com>
2016-11-19 15:23:50 -0600

    spring boot integration

---
+++ src/main/java/edu/mum/ea/crs/CrsApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.ea.crs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrsApplication.class, args);		
+	}
+	
+}

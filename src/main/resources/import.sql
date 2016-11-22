7ccb3b40c7b0ac1659f93d5b874a34b5b589a34b
roat167 <khemroat@gmail.com>
2016-11-22 08:46:15 -0600

    update sample input data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,4 @@
 --Sample input data
 	
-insert into eafp_db.car (manufacturer, car_model, plate_no, year, color, speed, rent_per_hour, status) 
-	values('Honda', 'Chevy', 'IA30343', 2011, 'Blue', 250, 4, 'AVAILABLE'); 
-	
-insert into eafp_db.car (manufacturer, car_model, plate_no, year, color, speed, rent_per_hour, status) 
-	values('Honda', 'CRV', 'IA30343', 2016, 'Green', 250, 5, 'AVAILABLE'); 
+insert into eafp_db.car (manufacturer, car_model, plate_no, year, color, speed, rent_per_hour, status) values('Honda', 'Chevy', 'IA30343', 2011, 'Blue', 250, 4, 'AVAILABLE');	
+insert into eafp_db.car (manufacturer, car_model, plate_no, year, color, speed, rent_per_hour, status) values('Honda', 'CRV', 'IA10340', 2016, 'Green', 200, 5, 'AVAILABLE'); 

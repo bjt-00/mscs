e2dc6bd7505e914f1521bc3e740863fe33eda152
roat167 <khemroat@gmail.com>
2016-11-22 08:20:34 -0600

    add sample data for car from script

--- src/main/java/edu/mum/ea/crs/controller/CarController.java
+++ src/main/java/edu/mum/ea/crs/controller/CarController.java
@@ -16,9 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.mum.ea.crs.data.domain.Car;
-import edu.mum.ea.crs.data.domain.User;
 import edu.mum.ea.crs.service.CarService;
-import edu.mum.ea.crs.service.UserService;
 
 @Controller
 @RequestMapping("/cars")
@@ -30,25 +28,11 @@ public class CarController extends GenericController {
 	private static final String VIEW_LIST = "car/carList";
 
 	@Autowired
-	private CarService carService;
-	@Autowired
-	private UserService userService;
-	private int count;
+	private CarService carService;	
 
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public String getAll(Model model, @ModelAttribute("project") Car c) {
 		logger.info("CarController getAll ");
-		Car car = new Car();
-		car.setCarModel("Honda");
-		car.setManufacturer("M001" + count++);
-		car.setColor("Blue");
-		car.setYear(2011);
-		car.setPlateNo("000000002");
-		car.setSpeed(200);
-		car.setStatus(Car.STATUS_AVAILABLE);
-		car.setRentPerHour(2.0);
-		carService.save(car);
-
 		model.addAttribute("cars", carService.findAll());
 		if (c.getId() == null) {
 			c = new Car();

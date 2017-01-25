4c88ac4d52c0635ace0aacc8b503d6373d857149
thesuffah <bitguiders@gmail.com>
2017-01-24 19:32:04 -0600

    Backing Bean implementation

--- src/main/java/mum/cs545/model/User.java
+++ src/main/java/mum/cs545/model/User.java
@@ -1,32 +1,59 @@
 package mum.cs545.model;
 
+import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
 
 @ManagedBean(name="user")
 public class User {
 
-	private String userName;
-	private String password;
 
+	private String userName_;
+	private String password_;
+	private String postConstructMessage;
+	private String preDestroyMessage;
+
+	@PostConstruct
+	public void init(){
+		postConstructMessage="Initiated :) ";
+	}
+	@PreDestroy
+	public void clean(){
+		postConstructMessage="Destroyed :( ";
+	}
+	
 	public User(){}
 	public User(String userName,String password){
-		this.userName = userName;
-		this.password = password;
+		this.userName_ = userName;
+		this.password_ = password;
 	}
 	public String getUserName() {
-		return userName;
+		return userName_;
 	}
 	public void setUserName(String userName) {
-		this.userName = userName;
+		this.userName_ = userName;
 	}
 	public String getPassword() {
-		return password;
+		return password_;
 	}
 	public void setPassword(String password) {
-		this.password = password;
+		this.password_ = password;
 	}
 	
 	public String getMessage(){
-		return "Welcome "+userName;
+		return (null!=userName_ && !"".equals(userName_) ?"Welcome "+userName_:"Please Enter User Name");
+		//UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
+		//viewRoot.setLocale(new Locale("en"));
+	}
+
+	public String getPostConstructMessage() {
+		return postConstructMessage;
+	}
+	public String getPreDestroyMessage() {
+		return preDestroyMessage;
 	}
 }

4ef1eb352674380a663dac5fc962631670bea3b1
fitsama <fitsum.admassu@gmail.com>
2017-03-08 16:17:17 -0600

    done

--- src/main/java/com/bitguiders/hadoop/projects/project1/stripes/StripesReducer.java
+++ src/main/java/com/bitguiders/hadoop/projects/project1/stripes/StripesReducer.java
@@ -1,19 +1,24 @@
 package com.bitguiders.hadoop.projects.project1.stripes;
 
+
 import java.io.IOException;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class StripesReducer extends Reducer<Text, IntWritable, Text, Text> {
+public class StripesReducer extends Reducer< Text,NeighborMap,Text,NeighborMap>
+{
+	@Override
+	public void reduce(Text keyword,Iterable<NeighborMap> neighborList, Context context) throws IOException, InterruptedException
+	{
+		NeighborMap intermidiateMap=new NeighborMap();
+		for(NeighborMap cuMap: neighborList)
+		{
+			intermidiateMap.mergeWithOtherFreqMap(cuMap);
+		}
+		NeighborMap outputMap= intermidiateMap.computeOcurrenceprobability();
+		context.write(keyword, outputMap);		
+	}
 
-    @Override
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+}
 
-    }
-    @Override
-    public void cleanup(Context context) throws IOException, InterruptedException{
-    }
-
-  }

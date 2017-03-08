f578b42faf775ba4169da15cfb77d0a3440bbb60
bitguiders <bitguiders@gmail.com>
2017-03-08 14:24:08 -0600

    centeral point to run all jobs

--- src/main/java/com/bitguiders/hadoop/projects/project1/RunJob.java
+++ src/main/java/com/bitguiders/hadoop/projects/project1/RunJob.java
@@ -1,7 +1,9 @@
 package com.bitguiders.hadoop.projects.project1;
 
+import java.io.File;
 import java.util.Scanner;
 
+import org.apache.hadoop.mapred.FileAlreadyExistsException;
 import org.apache.hadoop.util.ToolRunner;
 
 import com.bitguiders.hadoop.projects.project1.hybrid.HybridJob;
@@ -10,17 +12,44 @@ import com.bitguiders.hadoop.projects.project1.stripes.StripesJob;
 
 public class RunJob {
 	  public static void main(String args[]) throws Exception {
-		  
+		  try{
 		  start(args);
+		  }catch(FileAlreadyExistsException e){
+			  System.out.println(e.getMessage());
+		  }
 		  //System.exit(res);
 		  }
 	  public static void start(String args[]) throws Exception{
-		  System.out.println("Pair Job Id : 1\nStripes Job Id : 2");
-		  System.out.println("Hybrid Job Id : 3\nExit : 0");
+		  int jobId=4;
+	  
+		  //in case job param doens't exists
+		  if(null == args || args.length<2){
+			  System.out.println("Job Arguments missing ");
+			  System.out.println("Example:  input/mr.txt output 3");
 
-		  System.out.println("Enter Job id to rund");
+			  System.out.println("Please enter arguments now");
+			  Scanner scanner = new Scanner(System.in);
+			  String arg = scanner.nextLine();
+			  System.out.println("="+arg);
+			  args = arg.split(" ");
+			  jobId = Integer.parseInt(args[2]);
+		  }
+		  else if(args.length==2){
+		  System.out.println("Pair    Job Id : 1\nStripes Job Id : 2");
+		  System.out.println("Hybrid  Job Id : 3\nExit    Job Id : 0");
+
+		  System.out.println("Which Job you want to run..? Enter Job Id");
 		  Scanner scanner = new Scanner(System.in);
-		  int jobId = scanner.nextInt();
+		  jobId = scanner.nextInt();
+		  }else{
+			  jobId = Integer.parseInt(args[2]);			  
+		  }
+		  //delete output for fresh run
+		  File output = new File(args[1]);
+		  output.setWritable(true);
+		  output.delete();
+		  Thread.sleep(1000);
+		  
 		  int res=0;
 		  switch(jobId){
 		  case 0:
